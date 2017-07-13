eec51948c03f9d0cce391b88a9be1b15139edda8
Douglas Drouillard <drouillard@gmail.com>
2017-07-13 13:01:41 -0400

    move eth stats

--- architect/deploy-eth-stats.js
+++ architect/deploy-eth-stats.js
@@ -7,8 +7,21 @@ const wsSecret = require('../dashboard/config/globals.json').ethStatsPassword;
 const destinationFolder = path.join(__dirname, 'tmp', 'eth-stats');
 
 function installDependencies() {
-  console.log('Installing dependencies');
-  execSync('npm install && npm install -g grunt-cli && grunt', { cwd: destinationFolder });
+  // download eth stats
+  console.log('Downloading eth stats to ', destinationFolder);
+
+  exec(`git clone --depth=1 https://github.com/karalabe/eth-netstats ${destinationFolder}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    console.log('Installing dependencies');
+    execSync('npm install && npm install -g grunt-cli && grunt', { cwd: destinationFolder });
+    run();
+  });
 }
 
 function run() {
@@ -26,16 +39,3 @@ if (fs.existsSync(destinationFolder)) {
   installDependencies();
   run();
 }
-
-// download eth stats
-console.log('Downloading eth stats to ', destinationFolder);
-
-exec(`git clone --depth=1 https://github.com/karalabe/eth-netstats ${destinationFolder}`, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`exec error: ${error}`);
-    return;
-  }
-  console.log(`stdout: ${stdout}`);
-  console.log(`stderr: ${stderr}`);
-  run();
-});
